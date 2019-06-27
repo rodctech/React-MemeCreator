4ac46b4aad971e8c5d98a71edc6aa9c019cdb7fd
Rodrigo Chavez <rodctech@gmail.com>
2019-06-27 11:39:18 -0500

    Updates

---
+++ precache-manifest.e3d2750dcf97ebff7a42c88a876b1504.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46a4be565062ad26bd594417f212574f",
+    "url": "/React-MemeCreator/index.html"
+  },
+  {
+    "revision": "73f415de791d9155378a",
+    "url": "/React-MemeCreator/static/css/main.b7c94938.chunk.css"
+  },
+  {
+    "revision": "d8997ac44c043d25c8ee",
+    "url": "/React-MemeCreator/static/js/2.6ce11e82.chunk.js"
+  },
+  {
+    "revision": "73f415de791d9155378a",
+    "url": "/React-MemeCreator/static/js/main.73a1fc15.chunk.js"
+  },
+  {
+    "revision": "6505e71ab6cd7a40e62f",
+    "url": "/React-MemeCreator/static/js/runtime~main.2848598e.js"
+  }
+]);
